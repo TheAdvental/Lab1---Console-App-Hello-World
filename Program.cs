ae4032c04d37bee78cc86e4b11de53cd27e50c63
dp80025080@gmail.com <dp80025080@gmail.com>
2025-09-04 10:03:18 +0300

    Done.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Dmytro");
+for (int i = 1; i <= 100; i++)
+{
+    Console.WriteLine(i);
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FeezBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Feez");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+}
